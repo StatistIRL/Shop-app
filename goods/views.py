6de637d22edf3866f2562845245696eedeb7497e
StatistIRL <vazsry7@gmail.com>
2024-03-27 15:29:18 +0600

    Second pagination option - GET params

--- goods/views.py
+++ goods/views.py
@@ -7,7 +7,8 @@ from goods.models import Products
 # Create your views here.
 
 
-def catalog(request, category_slug, page=1):
+def catalog(request, category_slug):
+    page = request.GET.get("page", 1)
     if category_slug == "all":
         goods = Products.objects.all()
     else:
