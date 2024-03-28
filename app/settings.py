4314c09e40405bdf49eb9c6231a056bc4e08cc62
StatistIRL <vazsry7@gmail.com>
2024-03-28 16:30:02 +0600

    Templates for users app added

--- app/settings.py
+++ app/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     "django.contrib.postgres",
     "django_extensions",
     "debug_toolbar",
+    "users",
     "main",
     "goods",
 ]
@@ -62,7 +63,7 @@ ROOT_URLCONF = "app.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [],
+        "DIRS": [BASE_DIR / "templates"],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
